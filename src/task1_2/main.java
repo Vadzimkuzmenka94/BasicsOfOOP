f0ab8e281e30d44d80a6d69844a5c61e028478e3
37529 <VadzimKuzmenka@mail.ru>
2021-12-12 16:44:55 +0300

    task_complete

---
+++ src/task1_2/main.java
@@ -0,0 +1,19 @@
+package task1_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+Создать класс Payment с внутренним классом,
+с помощью объектов которого можно сформировать покупку из нескольких товаров.
+ */
+public class main {
+    public static void main(String[] args) throws IOException {
+        Payment payment = new Payment();
+        Payment.Order purchase = payment.new Order();
+        purchase.createShoppingList();
+        payment.amountMoneyPay(purchase.getOrderList());
+        payment.printShoppingList(purchase.getOrderList());
+    }
+}

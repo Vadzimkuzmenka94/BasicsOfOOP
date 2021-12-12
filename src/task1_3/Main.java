b233f2d89c87b97856322beb4c226fa179196879
37529 <VadzimKuzmenka@mail.ru>
2021-12-12 17:09:02 +0300

    task_complete

---
+++ src/task1_3/Main.java
@@ -0,0 +1,23 @@
+package task1_3;
+
+import java.text.ParseException;
+
+import static task1_3.Holiday.*;
+
+/*
+Задача 3.
+Создать класс Календарь с внутренним классом,
+с помощью объектов которого можно хранить информацию о выходных и праздничных днях.
+ */
+public class Main {
+    public static void main(String[] args) throws ParseException {
+        MyCalendar myCalendar = new MyCalendar("01-01-2021", "31-12-2021");
+        MyCalendar.WeekendAndHoliday weekendAndHoliday = myCalendar.new WeekendAndHoliday();
+        weekendAndHoliday.calcNumberWeekend(myCalendar.getStartDay(), myCalendar.getEndDay());
+        weekendAndHoliday.calcNumberHoliday(myCalendar.getStartDay(), myCalendar.getEndDay());
+        System.out.println("Временной интервал от " + df.format(myCalendar.getStartDay().getTime()) +
+                " до " + df.format(myCalendar.getEndDay().getTime()) + ':');
+        System.out.println("количество выходных дней - " + weekendAndHoliday.getNumberWeekend());
+        System.out.println("количество праздничных дней - " + weekendAndHoliday.getNumberHoliday());
+    }
+}
